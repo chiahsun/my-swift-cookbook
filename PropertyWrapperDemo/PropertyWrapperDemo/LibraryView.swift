e9e6c43d5c96f6005ff135006c50a3ee3f58e37b
chiahsun <chiahsun0814@gmail.com>
2021-08-06 23:48:11 +0800

    Add observable object published observed object and state object

---
+++ PropertyWrapperDemo/PropertyWrapperDemo/LibraryView.swift
@@ -0,0 +1,42 @@
+import SwiftUI
+
+class Book: ObservableObject {    // <1>
+    @Published var title = "Harry Potter" // <2>
+    let identifier = UUID()
+}
+
+struct BookView: View {
+    @ObservedObject var book: Book   // <3>
+    
+    var body: some View {
+        Text(book.title)
+    }
+}
+
+
+struct BookEditView: View {
+    @ObservedObject var book: Book   // <3>
+
+    var body: some View {
+        TextField("Edit", text: $book.title)
+            .multilineTextAlignment(.center)
+            
+    }
+}
+// https://developer.apple.com/documentation/swiftui/managing-model-data-in-your-app
+struct LibraryView: View {
+    @StateObject var book = Book()  // <4>
+    
+    var body: some View {
+        VStack(alignment: .center) {
+            BookView(book: book)
+            BookEditView(book: book)
+        }
+    }
+}
+
+struct LibraryView_Previews: PreviewProvider {
+    static var previews: some View {
+        LibraryView()
+    }
+}
