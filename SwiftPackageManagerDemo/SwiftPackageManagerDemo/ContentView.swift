3e40910ce4a6084289e1ccc4cac0f839d53e0ee1
chiahsun <chiahsun0814@gmail.com>
2021-08-15 12:58:25 +0800

    Add swift package manager demo

--- SwiftPackageManagerDemo/SwiftPackageManagerDemo/ContentView.swift
+++ SwiftPackageManagerDemo/SwiftPackageManagerDemo/ContentView.swift
@@ -1,6 +1,11 @@
 import SwiftUI
+import OrderedDictionary // <1>
 
 struct ContentView: View {
+    let dict: OrderedDictionary = [ // <2>
+        "a": 1,
+        "b": 2
+    ]
     var body: some View {
         Text("Hello, world!")
             .padding()
